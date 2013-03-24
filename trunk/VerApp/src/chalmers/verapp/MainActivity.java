ef0eaa4975f3b51e81da3d3a2cd125d343330705
joanna.m.eriksson@gmail.com <joanna.m.eriksson@gmail.com@e0f2cffa-44b0-c5ef-fe14-801b8d68650b>
2013-03-24 09:19:07 +0000

    containing some code to connect to a USB-device

--- trunk/VerApp/src/chalmers/verapp/MainActivity.java
+++ trunk/VerApp/src/chalmers/verapp/MainActivity.java
@@ -2,15 +2,21 @@ package chalmers.verapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	UsbDevice device;
 
 	// Start create
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);	//obtaining the USB-device
     }
 
     @Override
@@ -18,6 +24,10 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void onNewIntent(Intent i){
+    	//Kolla så att rätt USB-enhet är ansluten, Denna behövs ev ej
     }
     
 }
