aec56411f6c2ea2477e5772a17f22f80b7e47693
anders.t.nordin@gmail.com <anders.t.nordin@gmail.com@e0f2cffa-44b0-c5ef-fe14-801b8d68650b>
2013-03-25 15:43:16 +0000

    Design: background, button, logo added.

--- VerApp/src/chalmers/verapp/MainActivity.java
+++ VerApp/src/chalmers/verapp/MainActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
 import android.view.Menu;
+
 
 public class MainActivity extends Activity {
 	
