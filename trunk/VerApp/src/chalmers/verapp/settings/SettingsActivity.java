def0a3ff2e6b6074e1a3d020ef766e3f4d5c9e48
joanna.m.eriksson@gmail.com <joanna.m.eriksson@gmail.com@e0f2cffa-44b0-c5ef-fe14-801b8d68650b>
2013-04-16 09:01:12 +0000

    part of frequency settings added

---
+++ trunk/VerApp/src/chalmers/verapp/settings/SettingsActivity.java
@@ -0,0 +1,24 @@
+package chalmers.verapp.settings;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getFragmentManager().beginTransaction()
+		.replace(android.R.id.content, new SettingsFragment()).commit();
+
+	}
+
+	public static void choseFreq() {
+		//SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		//setLogFreq(sharedPrefs.getString("frequency", "NULL"));	//Inte bra att funktionen har string som parameter, ska vara int! Skapa metoden setLogFreq där frekvensvariabeln används.Det här behöver även skrivas om lite grann. Kanske ha någon form av case sats för att settings ska kunna utökas senare.  
+
+	}
+
+}
