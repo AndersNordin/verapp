0cdb5c3179feaf47a1cc6a57a34ff64f6391e0ca
anders.t.nordin@gmail.com <anders.t.nordin@gmail.com@e0f2cffa-44b0-c5ef-fe14-801b8d68650b>
2013-05-02 11:52:11 +0000

    Async task now works on Xperia Sola.

--- VerApp/src/chalmers/verapp/DatabaseManager.java
+++ VerApp/src/chalmers/verapp/DatabaseManager.java
@@ -3,6 +3,8 @@ package chalmers.verapp;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -12,26 +14,27 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
-public class DatabaseManager extends AsyncTask<String, Void, Void>{
-
+public class DatabaseManager extends AsyncTask<String, Void, Boolean>{
+	boolean success = false;
 	@Override
 	/*
 	 * Param1=signalName,Param2=signalValue,Param3=lon, Param4=lat,Param5=warning
 	 * @see android.os.AsyncTask#doInBackground(Params[])
 	 */
-	protected Void doInBackground(String... params) {
-		
-		String signalName = params[0];
-		String signalValue = params[1];
-		String lon = params[2];
-		String lat = params[3];
-		String warning = params[4];
-		
+	protected Boolean doInBackground(String... strings) {
+
+		String signalName = strings[0];
+		String signalValue = strings[1];
+		String lon = strings[2];
+		String lat = strings[3];
+		String warning = strings[4];
+
 		// Create a new HttpClient and Post Header
 		HttpClient httpclient = new DefaultHttpClient();
-		
-		HttpPost httppost = new HttpPost("http://chalmersverateam.se/verapp/dbManager.php");  
+
+		HttpPost httppost = new HttpPost("http://chalmersverateam.se/XMLData.php");  
 		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 
 		try {
@@ -41,23 +44,25 @@ public class DatabaseManager extends AsyncTask<String, Void, Void>{
 			nameValuePairs.add(new BasicNameValuePair("long", lon));
 			nameValuePairs.add(new BasicNameValuePair("lat", lat));
 			nameValuePairs.add(new BasicNameValuePair("warning", warning));
-			
-			/* Log.d("signalName", signalName);
-			Log.d("signalValue", signalValue);
-			Log.d("long", lon);
-			Log.d("lat", lat);
-			Log.d("warning", warning); */
 
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
 			// Execute HTTP Post Request
-			/* HttpResponse response =*/ httpclient.execute(httppost);
+			HttpResponse response = httpclient.execute(httppost);
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return null;
-		
+		success = true;
+
+		return success;
 	}	
+
+	@Override
+	protected void onPostExecute(Boolean result) {
+		if (result)
+			Log.i("ASYNC SENDING SUCCESS", "" + result);
+
+	}
 }
