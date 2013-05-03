92dbc05747c1fd46a26b9874d944c9c3679c38bf
anders.t.nordin@gmail.com <anders.t.nordin@gmail.com@e0f2cffa-44b0-c5ef-fe14-801b8d68650b>
2013-05-03 07:54:53 +0000

    small fixes

--- VerApp/src/chalmers/verapp/DatabaseManager.java
+++ VerApp/src/chalmers/verapp/DatabaseManager.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -12,9 +11,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-
 import android.os.AsyncTask;
-import android.util.Log;
 
 public class DatabaseManager extends AsyncTask<String, Void, Boolean>{
 	boolean success = false;
@@ -50,7 +47,7 @@ public class DatabaseManager extends AsyncTask<String, Void, Boolean>{
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
 			// Execute HTTP Post Request
-			HttpResponse response = httpclient.execute(httppost);
+			/* HttpResponse response = */ httpclient.execute(httppost);
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
